82880b1cee704570d0144397bb6bd1fbf103e0ec
fearless-command-2358 <iuriseara@gmail.com>
2020-04-26 19:18:05 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,13 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-def display_board(board)
 
+def display_board(board)
+  board = [" "," "," "," "," "," "," "," "," "]
+  row = "|"
+  line = "-----------"
+  puts "#{board[0]} #{row} #{board[1]} #{row} #{board[2]}"
+  puts "#{line}"
+  puts "#{board[3]} #{row} #{board[4]} #{row} #{board[5]}"
+  puts "#{line}"
+  puts "#{board[6]} #{row} #{board[7]} #{row} #{board[8]}"
 end
