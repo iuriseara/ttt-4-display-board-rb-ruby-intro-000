b1d95728f6cb7287c8ecb3ffee931b5e6ae6e58c
fearless-command-2358 <iuriseara@gmail.com>
2020-04-26 19:03:11 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,5 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(board)
-  board = [" "," "," "," "," "," "," "," "," "]
-  output = "capture_puts#{display_board(board)}"
-  rows = output.split(" ")
-  puts "#{board}"
 
-  puts rows[0]
 end
